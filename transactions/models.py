9e7a110f4524be6c041e15547549d396eb4cae97
kelvin <kellzer01@gmail.com>
2026-05-28 08:43:50 -0300

    feat: initial Django REST API setup with Transaction model

---
+++ transactions/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Transaction(models.Model):
+  name = models.CharField(max_length=255)
+  value = models.DecimalField(max_digits=10, decimal_places=2)
+  date = models.DateField()
+  type = models.CharField(max_length=10)
+  status = models.CharField(max_length=10)
+
+  def __str__(self):
+    return self.name
