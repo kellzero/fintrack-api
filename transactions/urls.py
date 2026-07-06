f4ea553c1a108b00290629996e98a059f56e8f62
kelvin <kellzer01@gmail.com>
2026-07-06 09:55:42 -0300

    feat: add register endpoint

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,7 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import TransactionViewSet
+from .views import TransactionViewSet, register
+from django.urls import path
 
 router = DefaultRouter()
 router.register(r'transactions', TransactionViewSet, basename='transaction')
 
-urlpatterns = router.urls
+urlpatterns = router.urls +[
+    path('register/', register),
+]
--- transactions/views.py
+++ transactions/views.py
@@ -1,4 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from django.contrib.auth.models import User
 from .models import Transaction
 from .serializers import TransactionSerializer
 
@@ -7,3 +11,19 @@ from .serializers import TransactionSerializer
 class TransactionViewSet(viewsets.ModelViewSet):
   queryset = Transaction.objects.all()
   serializer_class = TransactionSerializer
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+
+def register(request):
+  username= request.data.get('username')
+  password= request.data.get('password')
+
+  if not username or not password:
+    return Response({'error': 'Please provide both username and password'}, status=status.HTTP_400_BAD_REQUEST)
+  
+  if User.objects.filter(username=username).exists():
+    return Response({'error': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST) 
+  
+  User.objects.create_user(username=username, password=password)
+  return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
